12a222c9629ad82fe9720aaeb7da76261be99f19
Calvin C Chan <hello@calvin-c.com>
2024-05-19 07:45:34 -0700

    chore: improve visual feedback for long running processes

--- index.ts
+++ index.ts
@@ -1,53 +1,61 @@
-import { Ollama } from "@langchain/community/llms/ollama";
 import { RetrievalQAChain } from "langchain/chains";
 import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
 import { TextLoader } from "langchain/document_loaders/fs/text";
 import { OllamaEmbeddings } from "langchain/embeddings/ollama";
+import { Ollama } from "langchain/llms/ollama";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import ora from "ora";
 import readline from "readline";
 
 const LOGSEQ_JOURNAL_PATH = "./journals";
 
 async function main() {
+  const stage0 = ora("Loading Journals").start();
   const docs = await importJournals();
+  stage0.succeed();
 
   /** Prepare LLM model with tools */
-  console.log("Loading LLM model...");
+  const stage1 = ora("Loading LLM model").start();
   const model = new Ollama({
     baseUrl: "http://127.0.0.1:11434",
     model: "llama3",
     temperature: 0.001,
   });
+  stage1.succeed();
 
   /** Create a MemoryVectorStore from docs for local ollama */
-  console.log("Creating MemoryVectorStore...");
+  const stage2 = ora("Creating MemoryVectorStore").start();
   const store = await MemoryVectorStore.fromDocuments(
     docs,
     new OllamaEmbeddings({ model: "llama3" })
   );
+  stage2.succeed();
 
   /** QA Chain */
-  console.log("Creating QAChain...");
+  const stage3 = ora("Creating QAChain").start();
   const chain = RetrievalQAChain.fromLLM(model, store.asRetriever());
+  stage3.succeed();
 
   /** Interactive prompt */
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  while (true) {
+  for (;;) {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
     const query = await new Promise((resolve) => {
-      rl.question('🤖 Enter your query (or "exit" to quit): ', (answer) =>
-        resolve(answer)
-      );
+      rl.question('🤖 Enter your query (or "exit" to quit): ', (answer) => {
+        rl.close();
+        resolve(answer);
+      });
     });
 
     if (query === "exit") {
       break;
     }
 
-    console.log("🤖 Thinking...");
+    const spinner = ora("Thinking...").start();
     const res = await chain.invoke({ query });
+    spinner.stop();
     console.log("✨ Answer:");
     console.log(res.text);
   }
